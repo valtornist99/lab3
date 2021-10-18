289cc22cca57c17916da6e7ac7933e195d968d48
Eduard Glekler <eduard.glekler@yandex.ru>
2021-10-18 11:23:01 +0300

    final

--- SmartM3_Aux_Robot/main.py
+++ SmartM3_Aux_Robot/main.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import random
 import time
 from smart_m3.m3_kp_api import *
@@ -5,33 +6,70 @@ from smart_m3.m3_kp_api import *
 robotID = "robot_1"
 auxRobotID = "aux_" + robotID
 
-def askForTarget(kp):
-    kp.load_rdf_remove(Triple(URI("goal_" + auxRobotID), URI("is"), URI("find_target")))
-    kp.load_rdf_insert(Triple(URI("goal_" + auxRobotID), URI("is"), URI("find_target")))
+targetCoordinates = [10, 20, 35, 45]
+curTarget = 0
 
-def move(target_coordinate, kp):
-    kp.load_query_rdf(Triple(URI("robot_coordinate_" + robotID), URI("is"), None))
-    res = kp.result_rdf_query
-    robot_coordinate = int(str(res[0][2]))
-    distance = target_coordinate - robot_coordinate
-    time.sleep(random.randint(1, 5))
-    print(robotID + " moved " + str(distance) + " meters")
-    new_robot_coordinate = robot_coordinate + distance
-    kp.load_rdf_remove(Triple(URI("robot_coordinate_" + robotID), URI("is"), None))
-    kp.load_rdf_insert(Triple(URI("robot_coordinate_" + robotID), URI("is"), Literal(new_robot_coordinate)))
+hits = 0
+timer = 0
+timerStopped = True
 
-def shoot(kp):
+def StartTimer():
+    global timer
+    while True:
+        if not timerStopped:
+            time.sleep(1)
+            timer += 1
+timer_thread = Thread(target=StartTimer, args=())
+
+def findTarget(kp):
+    global curTarget
+    global timer_thread
+    global timerStopped
+
+    if not timer_thread.is_alive():
+        timer_thread.start()
+
+    timerStopped = True
+    print("time: " + str(timer))
+
     time.sleep(random.randint(1, 5))
-    print(robotID + " turned around")
-    print(robotID + " shot")
-    print(robotID + " turned around")
-    kp.load_rdf_remove(Triple(URI("goal_" + auxRobotID), URI("is"), URI("evaluate_shot")))
-    kp.load_rdf_insert(Triple(URI("goal_" + auxRobotID), URI("is"), URI("evaluate_shot")))
+    if curTarget != len(targetCoordinates) - 1:
+        kp.load_rdf_remove(Triple(URI("target_coordinate_" + robotID), URI("is"), None))
+        kp.load_rdf_insert(Triple(URI("target_coordinate_" + robotID), URI("is"), Literal(targetCoordinates[curTarget])))
+    else:
+        kp.load_rdf_remove(Triple(URI("end_of_track_coordinate_" + robotID), URI("is"), None))
+        kp.load_rdf_insert(Triple(URI("end_of_track_coordinate_" + robotID), URI("is"), Literal(targetCoordinates[curTarget])))
+    curTarget += 1
+
+    timerStopped = False
+
+def evaluateShot(kp):
+    global hits
+    global timerStopped
+    timerStopped = True
+    print("time: " + str(timer))
+
+    time.sleep(random.randint(1, 5))
+    hit = random.randint(0, 1)
+    hits += hit
+
+    kp.load_rdf_remove(Triple(URI("shot_" + robotID), URI("is"), URI("evaluated")))
+    kp.load_rdf_insert(Triple(URI("shot_" + robotID), URI("is"), URI("evaluated")))
+
+    timerStopped = False
 
 def end(kp):
-    print(robotID + " finished!")
-    kp.load_rdf_remove(Triple(URI(robotID), URI("has_been_reached"), URI("end_of_track")))
-    kp.load_rdf_insert(Triple(URI(robotID), URI("has_been_reached"), URI("end_of_track")))
+    global timerStopped
+    timerStopped = True
+
+    kp.load_rdf_remove(Triple(URI("hits_result_" + robotID), URI("is"), None))
+    kp.load_rdf_insert(Triple(URI("hits_result_" + robotID), URI("is"), Literal(hits)))
+
+    kp.load_rdf_remove(Triple(URI("time_result_" + robotID), URI("is"), None))
+    kp.load_rdf_insert(Triple(URI("time_result_" + robotID), URI("is"), Literal(timer)))
+
+    print("Send result:" + str(hits) + ", " + str(timer))
+
 
 class Target_Handler:
     def __init__(self, kp=None):
@@ -39,9 +77,16 @@ class Target_Handler:
 
     def handle(self, added, removed):
         for data in added:
-            coordinate = int(str(data[2]))
-            move(coordinate, kp)
-            shoot(kp)
+            findTarget(kp)
+
+
+class Evaluation_Handler:
+    def __init__(self, kp=None):
+        self.kp = kp
+
+    def handle(self, added, removed):
+        for data in added:
+            evaluateShot(kp)
 
 
 class EndOfTrack_Handler:
@@ -49,40 +94,27 @@ class EndOfTrack_Handler:
         self.kp = kp
 
     def handle(self, added, removed):
+        time.sleep(3)
         for data in added:
-            coordinate = int(str(data[2]))
-            move(coordinate, kp)
             end(kp)
 
-
-class Shot_Handler:
-    def __init__(self, kp=None):
-        self.kp = kp
-
-    def handle(self, added, removed):
-        for data in added:
-            askForTarget(kp)
 
 if __name__ == '__main__':
     kp = m3_kp_api(PrintDebug=True)
 
-    kp.load_rdf_insert(Triple(URI("robot_coordinate_" + robotID), URI("is"), Literal(0)))
-
-    subscription_triple = Triple(URI("target_coordinate_" + robotID), URI("is"), None)
+    subscription_triple = Triple(URI("goal_" + auxRobotID), URI("is"), URI("find_target"))
     handler_target = Target_Handler(kp)
     handler_subscription1 = kp.load_subscribe_RDF(subscription_triple, handler_target)
 
-    subscription_triple = Triple(URI("end_of_track_coordinate_" + robotID), URI("is"), None)
+    subscription_triple = Triple(URI("goal_" + auxRobotID), URI("is"), URI("evaluate_shot"))
+    handler_evaluation = Evaluation_Handler(kp)
+    handler_subscription2 = kp.load_subscribe_RDF(subscription_triple, handler_evaluation)
+
+    subscription_triple = Triple(URI(robotID), URI("has_been_reached"), URI("end_of_track"))
     handler_endOfTrack = EndOfTrack_Handler(kp)
-    handler_subscription2 = kp.load_subscribe_RDF(subscription_triple, handler_endOfTrack)
+    handler_subscription3 = kp.load_subscribe_RDF(subscription_triple, handler_endOfTrack)
 
-    subscription_triple = Triple(URI("shot_" + robotID), URI("is"), URI("evaluated"))
-    handler_shot = Shot_Handler(kp)
-    handler_subscription3 = kp.load_subscribe_RDF(subscription_triple, handler_shot)
-
-    time.sleep(3)
-    askForTarget(kp)
-    time.sleep(600)
+    input("...")
 
     kp.load_unsubscribe(handler_subscription1)
     kp.load_unsubscribe(handler_subscription2)
